bca909aa6dc3f9d86c8be6b42c501c997b5fe42f
alvinoalvin <alvinoalvin@gmail.com>
2021-05-26 16:59:59 -0700

    using env file

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+   http_basic_authenticate_with name: ENV['admin_username'].to_s , password: ENV['admin_password'].to_s
   def show
   end
 end
