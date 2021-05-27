acd2b68cf3ca6ba58061d7e1b382ef1637c2c838
alvinoalvin <alvinoalvin@gmail.com>
2021-05-27 12:03:15 -0700

    Categories Fixed New category button

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -10,8 +10,8 @@ class Admin::CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
-    if @product.save
-      redirect_to [:admin, :products], notice: 'Category created!'
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
