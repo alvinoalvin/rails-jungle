8e0fd79f90e7eb53751dfa247ef700cb96d66c8a
alvinoalvin <alvinoalvin@gmail.com>
2021-06-01 15:40:16 -0700

    authenticate with credentials user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :name, presence: true
   validates :password, presence: true
+   
+  def authenticate_with_credentials (email,password) 
+    user = User.where("lower(email) = ?", email.downcase).first
+
+    if user != nil && user.authenticate(password)
+      return user
+    else
+      return nil
+    end
+  end
 end
