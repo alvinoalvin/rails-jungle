95ceecd156d6fa72f1555e01ac9fae611f91240d
alvinoalvin <alvinoalvin@gmail.com>
2021-06-01 19:23:34 -0700

    adding create for products

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -19,6 +19,13 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
   scenario "They see all products" do
     visit root_path
 
+    @category.products.create!(
+    name:  Faker::Hipster.sentence(3),
+    description: Faker::Hipster.paragraph(4),
+    image: open_asset('apparel1.jpg'),
+    quantity: 10,
+    price: 64.99
+    )
     # commented out b/c it's for debugging only
     # save_and_open_screenshot
 
