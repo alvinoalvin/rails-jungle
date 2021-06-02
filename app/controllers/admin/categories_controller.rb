81c18e44416f730748ab25bb8cffeb321192df91
alvinoalvin <alvinoalvin@gmail.com>
2021-06-02 13:23:37 -0700

    adding auth to categories controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'].to_s , password: ENV['ADMIN_PASSWORD'].to_s
+
   def index
     @category = Category.order(id: :desc).all
 
